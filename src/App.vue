76cee78c0238e964fef1149427e5c06b7b0a1f71
Khal3dAMARI <khaled.amari@novhadrim.com>
2022-01-14 22:28:18 +0100

    Re-wok

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
-    <div class="header">
-      <h1>Player: {{ player }}</h1>
-      <button @click.prevent="reset_game">Reset</button>
+    <table>
+      <tr>
+        <td class="box" id="0"></td>
+        <td class="box" id="1"></td>
+        <td class="box" id="2"></td>
+      </tr>
+      <tr>
+        <td class="box" id="3"></td>
+        <td class="box" id="4"></td>
+        <td class="box" id="5"></td>
+      </tr>
+      <tr>
+        <td class="box" id="6"></td>
+        <td class="box" id="7"></td>
+        <td class="box" id="8"></td>
+      </tr>
+    </table>
+    <div class="endgame">
+      <div class="text"></div>
     </div>
-    <div id="battle_field">
-      <div v-for="row in 3" :key="row" class="line">
-        <button
-          v-for="col in 3"
-          :key="col"
-          :id="'box' + row + col"
-          class="box"
-          @click.prevent="play"
-        ></button>
-      </div>
-    </div>
+    <button @click="startGame">Replay</button>
   </div>
 </template>
 
@@ -28,31 +34,75 @@ export default {
   },
   data() {
     return {
-      player: "X",
-      mark: "",
+      humanPlayer: "X",
+      computerPlayer: "O",
+      orignalBoard: [],
+      boxs: [],
+      winCombination: [
+        [0, 1, 2],
+        [0, 3, 6],
+        [0, 4, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
     };
   },
+  mounted() {
+    this.boxs = document.querySelectorAll(".box");
+    this.startGame();
+  },
   methods: {
-    play(e) {
-      e.target.innerText = this.player;
-      // e.target.outerText = this.player;
-      e.target.disabled = true;
-      this.player === "X" ? (this.player = "O") : (this.player = "X");
-    },
-    reset_game() {
-      const boxs = document.getElementsByClassName("box");
-      console.log(boxs);
-      for(let box of boxs) {
-        box.innerText = '';
-        box.disabled = false;
+    startGame() {
+      document.querySelector(".endgame").style.display = "none";
+      this.orignalBoard = Array.from(Array(9).keys());
+      for (let box of this.boxs) {
+        box.innerText = "";
+        box.style.removeProperty("background-color");
+        box.addEventListener("click", this.turnClick, false);
       }
-    }
+    },
+    turnClick(box) {
+      this.turn(box.target.id);
+    },
+    turn(boxId) {
+      this.orignalBoard[boxId] = this.humanPlayer;
+      document.getElementById(boxId).innerText = this.humanPlayer;
+      let gameWon = this.checkWin();
+      if(gameWon) {
+        this.gameOver(gameWon);
+      }
+    },
+
+    checkWin() {
+      let plays = this.orignalBoard.reduce((acc, e, i) => (e === this.humanPlayer) ? [...acc,i] : acc, []);
+      let gameWon = null;
+      for(let [index, element] of this.winCombination.entries()) {
+        if (element.every(e => plays.indexOf(e) > -1)) {
+          gameWon = {index: index, player: this.humanPlayer};
+          break;
+        }
+      }
+      return gameWon;
+    },
+
+    gameOver(gameWon) {
+      for (let index of this.winCombination[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor = 
+        gameWon.player == this.humanPlayer ? "lightBlue" : "red";
+      }
+      for (let box of this.boxs) {
+        box.removeEventListener('click', this.turnClick, false);
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -63,33 +113,62 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-}
+} */
 
 .header {
   width: 50%;
   display: flex;
   justify-content: space-between;
 }
-#battle_field {
-  background-color: lightblue;
-  width: 50%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
+
+td {
+  border: 2px solid #333;
+  height: 100px;
+  width: 100px;
+  text-align: center;
+  vertical-align: middle;
+  font-family: "Comic Sans MS", cursive, sans-serif;
+  font-size: 70px;
+  cursor: pointer;
 }
 
-.box {
+table {
+  border-collapse: collapse;
+  position: absolute;
+  left: 50%;
+  margin-left: -155px;
+  top: 50px;
+}
+
+table tr:first-child td {
+  border-top: 0;
+}
+
+table tr:last-child td {
+  border-bottom: 0;
+}
+
+table tr td:last-child {
+  border-right: 0;
+}
+
+table tr td:first-child {
+  border-left: 0;
+}
+
+.endgame {
+  display: none;
+  width: 200px;
+  top: 120px;
+  background-color: rgba((205, 133, 63, 0.8), green, blue, alpha);
+  position: absolute;
+  left: 50%;
+  margin-left: -100px;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  text-align: center;
+  border-radius: 5px;
   color: white;
-  font-size: 5rem;
-  width: 100%;
-  height: 100%;
-  border: 1px solid red;
-}
-
-.line {
-  display: flex;
-  flex-wrap: nowrap;
-  width: 100%;
-  height: 100%;
+  font-size: 2em;
 }
 </style>
